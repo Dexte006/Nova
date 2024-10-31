f0e44f1ac98f64645180d314b80c25f121d9b8b3
Rupam Dolui <157722068+Dexte006@users.noreply.github.com>
2024-10-31 13:51:14 +0530

    Add files via upload

---
+++ Nova/musicLibrary.py
@@ -0,0 +1,6 @@
+audio = {
+    "waterhouse": "https://youtu.be/wV1pBef4k44?si=sM6AGOCTf-nWEGm3",
+    "travis": "https://youtu.be/U-l4ya3ejko?si=6PTqDu--gHRVzy1x",
+    "keep": "https://youtu.be/I40yNUFGEH8?si=BaocqerkuyTPO0LS",
+    "metro": "https://youtu.be/Rr1Cdli5nE8?si=VCIMug5ZYhMtZSAO"
+}
